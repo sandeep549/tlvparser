1f714e7c64d78c412edf1f8fae9224bfe25651db
Sandeep K <sandeepkumar.dev@gmail.com>
2018-02-03 16:57:17 +0530

    There must be input source(e.g. stdin, network) from command line.

--- src/tlvprocessor/Main.java
+++ src/tlvprocessor/Main.java
@@ -6,10 +6,13 @@ import input.InputHandlerFactory;
 public class Main {
 
 	public static void main(String[] args) {
-		String inputType = args[0];
-
-		InputHandler handler = InputHandlerFactory.get(inputType);
-		handler.handle(inputType);
+		if (args.length > 0) {
+			String inputType = args[0];
+			InputHandler handler = InputHandlerFactory.get(inputType);
+			handler.handle(inputType);
+		}else {
+			System.out.println("Please specifiy input source");
+		}
 
 	}
 
